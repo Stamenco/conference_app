1c075cd416a1af311c1455392903011c66139b64
StamenchoBogdanovski <43040250+Stamenco@users.noreply.github.com>
2020-09-25 10:30:38 -0400

    Add files via upload

---
+++ src/main/java/com/example/conferencedemo/controller/SpeakersController.java
@@ -0,0 +1,45 @@
+package com.example.conferencedemo.controller;
+
+import com.example.conferencedemo.model.Speaker;
+import com.example.conferencedemo.repository.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Lob;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list(){
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody Speaker speaker){
+       return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id,@RequestBody Speaker speaker){
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+}
---
+++ src/main/java/com/example/conferencedemo/repository/SessionRepository.java
@@ -0,0 +1,7 @@
+package com.example.conferencedemo.repository;
+
+import com.example.conferencedemo.model.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+}
